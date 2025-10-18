2a839fd7a2eaa136dafbbd5edd7d2dbf19f719cc
Hallherout <danisdaffa8@gmail.com>
2025-10-18 11:42:48 +0700

    Add reset button

--- script.js
+++ script.js
@@ -17,21 +17,37 @@ function getPosition(element) {
     return new Position(parseInt(element.dataset.row), parseInt(element.dataset.col));
 }
 
-//Inisialisasi grid
-const row = 20,
-    column = 20,
-    table = document.querySelector('table');
+//Variabel grid
+let row = 20,
+    col = 20;
+const table = document.querySelector('table');
 
-for (let i = 0; i < row; i++) {
-    const tr = document.createElement('tr');
-    for (let j = 0; j < column; j++) {
-        const td = document.createElement('td');
-        td.dataset.row = i;
-        td.dataset.col = j;
-        tr.appendChild(td);
+//Fungsi untuk hapus grid
+function delGrid() {
+    while(table.firstChild) {
+        table.removeChild(table.firstChild);
     }
-    table.appendChild(tr);
 }
+
+//Fungsi untuk generate grid
+function genGrid(row, col) {
+    for (let i = 0; i < row; i++) {
+        const tr = document.createElement('tr');
+        for (let j = 0; j < col; j++) {
+            const td = document.createElement('td');
+            td.dataset.row = i;
+            td.dataset.col = j;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+} genGrid(row, col);
+
+//Event untuk reset grid
+document.getElementById('reset').addEventListener('click', () => {
+    delGrid();
+    genGrid(row, col);
+});
 
 //Mode selector
 let mode = 'start',
@@ -100,8 +116,8 @@ document.getElementById('vis').addEventListener('click', (e) => {
 
     //Definisi variabel BFS
     const queue = [start],
-        visited = Array.from({ length: row }, () => new Array(column).fill(false)),
-        parent = Array.from({ length: row }, () => new Array(column)),
+        visited = Array.from({ length: row }, () => new Array(col).fill(false)),
+        parent = Array.from({ length: row }, () => new Array(col)),
         direction = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
     let current;
@@ -137,7 +153,7 @@ document.getElementById('vis').addEventListener('click', (e) => {
             direction.forEach((x) => {
                 const child = new Position(current.row + x[0], current.col + x[1]);
 
-                if (child.row >= 0 && child.row < row && child.col >= 0 && child.col < column && !visited[child.row][child.col] && !getCell(child.row, child.col).classList.contains('wall')) {
+                if (child.row >= 0 && child.row < row && child.col >= 0 && child.col < col && !visited[child.row][child.col] && !getCell(child.row, child.col).classList.contains('wall')) {
                     queue.push(child);
                     parent[child.row][child.col] = current;
                     visited[child.row][child.col] = true;
